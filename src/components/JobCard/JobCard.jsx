a7ed0b659d1817b386245b47feb3a622d2a1b020
Hard1stf <hardikvijeta2@gmail.com>
2026-05-23 02:13:56 +0530

    feat(job-board): create reusable JobGrid and JobCard components, and make it interactive, job listing UI with skill tags and metadata

---
+++ src/components/JobCard/JobCard.jsx
@@ -0,0 +1,41 @@
+import { FaRegClock } from "react-icons/fa";
+
+
+const JobCard = ({ job }) => {
+  return (
+    <>
+      <div className="bg-slate-900 h-full flex flex-col items-start gap-4 hover:shadow-md hover:shadow-slate-700 transition-all duration-500 ease-in-out hover:-translate-y-2 rounded-2xl p-4 text-slate-400 border">
+        <div className="w-full flex justify-between"> 
+          <div className="bg-black flex items-center gap-2 text-white px-3 py-1 rounded-2xl">
+            <FaRegClock />
+            <span>{job.posted}</span>
+          </div>
+          <p className="bg-slate-950 text-white border uppercase font-semibold text-md px-2 py-1 rounded-2xl flex items-center gap-3">
+          <span className="relative flex size-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+            <span>{job.location}</span>
+          </p>
+        </div>
+        <div className="w-full flex flex-col flex-grow">
+          <div>
+            <h3 className="text-xl font-semibold"><span className="text-wrap text-white uppercase">{job.role}</span></h3>
+            <div className="flex gap-2 text-slate-200 font-semibold">
+              <h1><span>{job.company}</span></h1>•<span className="font-normal">{job.type}</span>
+            </div>
+          </div>
+          <div className="my-4 flex flex-col gap-4">
+            <p>{job.salary}</p>
+            <p className="flex flex-wrap gap-2">
+            {job.skills.map((skill,index) => <span className="font-semibold text-md px-4 py-1 rounded-2xl bg-slate-800 text-white" key={index}>{skill}</span>)}
+            </p>
+          </div>
+        <button className="mt-auto px-3 py-2 bg-slate-400 text-slate-900 font-semibold rounded-2xl">Apply now</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobCard;
---
+++ src/components/JobGrid/JobGrid.jsx
@@ -0,0 +1,31 @@
+import JobCard from '../JobCard/JobCard';
+
+const JobGrid = ({jobs}) => {
+
+  if(jobs.length === 0){
+    return(
+      <>
+        <div className='text-center py-20 text-slate-400'>
+          <h2 className='text-3xl font-semibold'>No Job found</h2>
+          <p className='mt-2'>
+            Try adjusting your search and filter.
+          </p>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+    <div className='max-w-7xl mx-auto px-4 py-10'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-10 items-stretch">
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job}/>
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default JobGrid;
