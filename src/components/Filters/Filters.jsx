aa5f083362a65b36a98d6f19c2155d3a0f0581ab
Hard1stf <hardikvijeta2@gmail.com>
2026-05-23 21:27:54 +0530

    style(Filters.jsx): fix the layout and the background-color

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,19 +1,17 @@
-const Filters = ({ selectedType, setSelectedType}) => {
+const Filters = ({ selectedType, setSelectedType }) => {
   const filtersByType = ['All', 'Internship', 'Full-time'];
 
   return (
     <>
-      <div className="">
-        <div className="max-w-md mx-auto flex justify-evenly rounded-full mt-10 text-lg">
-          {filtersByType.map((filter) => (
-            <button
-              onClick={() => setSelectedType(filter)}
-              className={`border font-semibold cursor-pointer bg-slate-800 py-2 px-4 rounded-full transition-all duration-300 ease-in-out ${selectedType === filter ? 'text-slate-900 bg-cyan-500' : 'text-white'}`}
-            >
-              {filter}
-            </button>
-          ))}
-        </div>
+      <div className="max-w-md mx-auto flex justify-evenly rounded-full mt-10 text-lg">
+        {filtersByType.map((filter) => (
+          <button
+            onClick={() => setSelectedType(filter)}
+            className={`border font-semibold cursor-pointe py-2 px-4 rounded-full transition-all duration-300 ease-in-out ${selectedType === filter ? 'text-slate-900 bg-cyan-500' : 'text-white'}`}
+          >
+            {filter}
+          </button>
+        ))}
       </div>
     </>
   );
