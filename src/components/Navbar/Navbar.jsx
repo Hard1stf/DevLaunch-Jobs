f08280d77350fe040fef217a6573addda38b7957
Hard1stf <hardikvijeta2@gmail.com>
2026-05-23 21:32:43 +0530

    perf(Navbar.jsx): Make Navbar more interactive and responsive for both samller and large screen size.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+import { HiAcademicCap } from 'react-icons/hi2';
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
-      <nav className="sticky z-50 top-5 max-w-7xl max-h-24 mx-auto px-4 py-4 border-2 border-zinc-700 rounded-xl backdrop-blur-xl text-yellow-50">
+      <nav className="sticky z-50 top-0 max-w-7xl mx-auto px-4 py-4 border-2 bg-slate-950/80 border-slate-800 rounded-b-xl backdrop-blur text-yellow-50">
         <div className="flex justify-between items-center">
-            <span>Logo</span> 
-            
-            <span>Desktop-Links</span> 
+          <div className="bg-white size-12 flex justify-center items-center rounded-full cursor-pointer">
+            <HiAcademicCap className="text-black text-4xl rotate-12 transition-all duration-500 hover:rotate-0" />
+          </div>
 
-            <span>Mobile-Menu-Button</span>
+          <div className="hidden md:flex items-center gap-3 font-semibold">
+            <span className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>Home</span>
+            <span className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>Saved Job</span>
+            <span className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>GitHub</span>
+          </div>
+
+          <div className="md:hidden flex gap-4 items-center">
+              <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? (
+                  <FaTimes className="size-6" />
+                ) : (
+                  <FaBars className="size-6" />
+                )}
+              </button>
+          </div>
         </div>
-        Mobile Menu
+        {isMenuOpen && (
+          <div className='md:hidden flex flex-col mt-4 gap-4 border-t border-slate-800 pt-4'>
+            <span className='cursor-pointer'>Home</span>
+            <span className='cursor-pointer'>Saved Job</span>
+            <span className='cursor-pointer'>GitHub</span>
+          </div>
+        )}
       </nav>
     </>
   );
