f40ba8f599bb245a1aaab4001116c8fbbedf9409
Hard1stf <hardikvijeta2@gmail.com>
2026-05-23 21:35:50 +0530

    perf(SearchBar.jsx): Make SearchBar more efficent to prevent the default browser action

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,9 @@
-import { FaSearch } from 'react-icons/fa';
+import { ImSearch } from "react-icons/im";
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
 
-  const handleSubmittedSearch = (searchTerm) => {
-    if (searchTerm === ""){
-      alert('No jobs found.\nTry adjusting your search or filters.')
-      setSearchTerm("");
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -14,22 +11,17 @@ const SearchBar = ({ searchTerm, setSearchTerm }) => {
       <form
         id=""
         name=""
-        className="max-w-sm border mx-auto p-2 flex justify-between rounded-full"
+        onSubmit={handleSubmit}
+        className="max-w-sm border mx-auto p-2 flex justify-between items-center rounded-full"
       >
+        <ImSearch className="text-3xl"/>
         <input
           placeholder="Search Role, Location, Company & Skills"
           type="search"
           value={searchTerm}
           onChange={e => setSearchTerm(e.target.value)}
-          className="transition-all outline-none w-80 bg-black px-4 py-2 text-white"
+          className="transition-all outline-none w-full bg-black px-4 py-2 text-white placeholder:italic"
         />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-zinc-800 text-white rounded-full"
-          onClick={() => handleSubmittedSearch(searchTerm) }
-        >
-          <FaSearch />
-        </button>
       </form>
     </>
   );
