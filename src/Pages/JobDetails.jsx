bf51c967c6d19b4e1878094a1f1404549f668904
Hard1stf <hardikvijeta2@gmail.com>
2026-05-25 18:46:00 +0530

    feat(JobDetails.jsx): create JobDetails view page with dynamic job route data

---
+++ src/Pages/JobDetails.jsx
@@ -0,0 +1,70 @@
+import { useParams } from 'react-router-dom';
+import { jobs } from '../data/jobs';
+import { IoTime, IoLocationSharp } from 'react-icons/io5';
+
+const JobDetails = () => {
+  const { id } = useParams();
+
+  const job = jobs.find((job) => job.id === Number(id));
+
+  return (
+    <>
+      <div className="w-full bg-black min-h-screen text-slate-100 flex justify-center">
+        <div className="w-1/3 h-fit mt-10 border-slate-700 border px-4 p-8 flex flex-col gap-4">
+          {/* Head section */}
+          <div className="flex flex-col gap-1 pb-8 border-b border-slate-700 text-slate-200 text-lg">
+            <h1 className="font-bold uppercase text-4xl">{job.role}</h1>
+            <div className="flex flex-col gap-1 items-start">
+              <div className="flex gap-2">
+                <span className="font-semibold">{job.company}</span>•
+                <span>{job.type}</span>
+              </div>
+              <div className="flex items-center gap-4">
+                <div className="w-fir flex py-1 px-3 justify-between border gap-2 rounded-full items-center">
+                  <IoTime />
+                  <span className="font-semibold text-base tracking-tighter">
+                    {job.posted}
+                  </span>
+                </div>
+                <div className="w-fit flex py-1 px-3 justify-between border gap-2 rounded-full items-center">
+                  <IoLocationSharp />
+                  <span className="font-semibold text-base tracking-tighter">
+                    {job.location}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Neck section */}
+          <div className="flex flex-col gap-2 border-b pb-8 border-slate-700">
+            <span className='font-semibold'>{job.salary}</span>
+            <p className='flex flex-wrap gap-2'>
+              {job.skills.map((skill) => (
+                <span className="border-2 font-semibold rounded-full py-2 px-4">{skill}</span>
+              ))}
+            </p>
+          </div>
+          {/* Body section */}
+          <div className="group flex flex-col gap-2">
+            <h2 className="font-semibold text-2xl">About the job</h2>
+            <p className="group-hover:border-l-2 pl-2">{job.description}</p>
+            <p className="ml-2 flex flex-col gap-2">
+              <span className="font-semibold text-lg">Requirements:</span>
+              {job.requirements.map((req) => (
+                <span className="transition-all duration-150 px-2 hover:border-l-2">
+                  {req}
+                </span>
+              ))}
+            </p>
+          </div>
+          {/* Lower Section */}
+          <div className='flex justify-center items-center gap-4'>
+            <button className="w-full mt-auto px-3 py-2 bg-slate-400 text-slate-900 font-semibold rounded-2xl">Apply now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobDetails;
