05857a6aabf40bce40461a6bc40ed1710f9bd079
Hard1stf <hardikvijeta2@gmail.com>
2026-05-25 18:32:01 +0530

    feat(App.jsx): set up app routing for home, job details, and saved jobs pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import JobDetails from "./pages/JobDetails";
+import SavedJobs from "./pages/SavedJobs";
 
 const App = () => {
   return(
     <>
-    <Home />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/jobs/:id" element={<JobDetails />}/>
+      <Route path="/saved-jobs" element={<SavedJobs />} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
