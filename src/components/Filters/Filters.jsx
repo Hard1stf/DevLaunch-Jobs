4b58cc938671a0159464a95fe1ff7fcbd684073b
Hard1stf <hardikvijeta2@gmail.com>
2026-05-25 18:22:45 +0530

    style(filters.jsx): change the border color from white to cyan

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -7,7 +7,7 @@ const Filters = ({ selectedType, setSelectedType }) => {
         {filtersByType.map((filter) => (
           <button
             onClick={() => setSelectedType(filter)}
-            className={`border font-semibold cursor-pointe py-2 px-4 rounded-full transition-all duration-300 ease-in-out ${selectedType === filter ? 'text-slate-900 bg-cyan-500' : 'text-white'}`}
+            className={`border font-semibold cursor-pointe py-2 px-4 rounded-full border-cyan-500 transition-all duration-300 ease-in-out ${selectedType === filter ? 'text-slate-900 bg-cyan-500' : 'text-white'}`}
           >
             {filter}
           </button>
