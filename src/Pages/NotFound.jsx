097e87c7afcf8dc64afa12531439ede91037c338
Hard1stf <hardikvijeta2@gmail.com>
2026-05-28 15:57:38 +0530

    perf(NotFound.jsx): Implement Layout make it section.

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,18 +1,23 @@
 import { IoRocketSharp } from "react-icons/io5";
 import NotFound404 from '../assets/NotFound-404.png';
+import Layout from "../components/layout/Layout";
 
 const NotFound = () => {
     return(
         <>
-            <div className='bg-black min-h-screen flex items-center justify-center'>
+        <Layout>
+            <section className='flex flex-col items-center justify-center px-4 py-20'>
                 <div className="relative group">
                     <img src={NotFound404} alt="404-not-found" className='max-w-2xl'/>
-                    <button className='absolute bottom-24 transition-all duration-500 group-hover:bg-pink-800 group-hover:text-white left-56 uppercase text-pink-800 font-semibold tracking-widest border-2 border-pink-800 flex items-center gap-3 px-4 py-2 rounded-lg'>
+                    <button className='absolute bottom-24 transition-all duration-500 group-hover:bg-pink-800 group-hover:text-white left-56 uppercase text-pink-800 font-semibold tracking-widest border-2 border-pink-800  px-4 py-2 rounded-lg'>
+                    <a href={`/`} className="flex items-center gap-3">
                         <IoRocketSharp className="text-2xl"/>
                         Go back Home
+                    </a>
                     </button>
                 </div>
-            </div>
+            </section>
+        </Layout>
         </>
     );
 }
