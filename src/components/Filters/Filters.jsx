5043b19bae1eab1cdad89cfed13effaa7e3cba9b
Hard1stf <hardikvijeta2@gmail.com>
2026-06-05 23:59:42 +0530

    fix(Filters.jsx): Use key prop to make button unique

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -7,7 +7,8 @@ const Filters = ({ selectedType, setSelectedType }) => {
     <>
       <div className="max-w-md mx-auto flex justify-evenly rounded-full mt-10 text-lg">
         {filtersByType.map((filter) => (
-          <Button
+          <Button 
+            key={filter}
             onClick={() => setSelectedType(filter)}
             className={`border font-semibold cursor-pointe py-2 px-4 rounded-full transition-all duration-300 ease-in-out ${selectedType === filter ? 'text-cyan-500 border-cyan-500' : 'text-white'}`}
             variant="secondary"
