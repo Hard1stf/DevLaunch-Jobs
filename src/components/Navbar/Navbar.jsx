06675db88724d98ca24a0e3864149c417a8dab25
Hard1stf <hardikvijeta2@gmail.com>
2026-06-05 21:58:55 +0530

    style/chore(Navbar.jsx): Implement active tab style via NavLink isActive prop.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { HiAcademicCap } from 'react-icons/hi2';
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navLinkStyle = ({isActive}) => isActive ? "text-cyan-400" : "hover:text-cyan-400 transition-colors duration-300";
 
   return (
     <>
       <nav className="sticky z-50 top-0 w-full border-b px-4 py-4 bg-slate-950/80 border-slate-800 backdrop-blur">
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-          <Link to={`/`} className="flex justify-center items-center rounded-full cursor-pointer">
+          <NavLink to="/" end className="flex justify-center items-center rounded-full cursor-pointer">
             <HiAcademicCap className="text-white text-4xl rotate-12 transition-all duration-500 hover:rotate-0" />
-          </Link>
+          </NavLink>
 
           <div className="hidden md:flex items-center gap-3 font-semibold">
-            <Link to={`/`} className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>Home</Link>
-            <Link to={`/saved-jobs`} className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>Saved Job</Link>
-            <a href={`https://github.com/Hard1stf`} target='_blank' rel="noopener noreferrer" className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>GitHub</a>
+            <NavLink to="/" className={navLinkStyle}>Home</NavLink>
+            <NavLink to="/saved-jobs" className={navLinkStyle}>Saved Job</NavLink>
+            <a href="https://github.com/Hard1stf" target='_blank' rel="noopener noreferrer" className='cursor-pointer hover:text-cyan-400 transition-color duration-300'>GitHub</a>
           </div>
 
           <div className="md:hidden flex gap-4 items-center">
@@ -32,9 +33,9 @@ const Navbar = () => {
         </div>
         {isMenuOpen && (
           <div className='md:hidden flex flex-col mt-4 gap-4 border-t border-slate-800 pt-4'>
-            <Link to={`/`} className='cursor-pointer'>Home</Link>
-            <Link to={`/saved-jobs`} className='cursor-pointer'>Saved Job</Link>
-            <a href={`https://github.com/Hard1stf`} target='_blank' rel="noopener noreferrer" className='cursor-pointer'>GitHub</a>
+            <NavLink to="/" className={navLinkStyle}>Home</NavLink>
+            <NavLink to="/saved-jobs" className={navLinkStyle}>Saved Job</NavLink>
+            <a href="https://github.com/Hard1stf" target='_blank' rel="noopener noreferrer" className='cursor-pointer'>GitHub</a>
           </div>
         )}
       </nav>
