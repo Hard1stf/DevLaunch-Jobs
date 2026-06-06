c73d802bc973888c281e1875703ec269fc9c6285
Hard1stf <hardikvijeta2@gmail.com>
2026-06-06 14:20:48 +0530

    style/UI(Hero.jsx): Make Hero title and description responsive, and hero-section visually clear

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,12 @@ import Button from "../UI/Button";
 const Hero = () => {
   return (
     <>
-      <div className="min-h-screen w-full flex-col flex items-center gap-20">
-        <div className="flex flex-col items-center gap-5 mt-40 text-zinc-300">
-          <h1 className="text-7xl font-bold">Find Your Next Frontend Internship</h1>
-          <h3 className="text-xl font-semibold">Discover internships and freelance opportunities built for developers.</h3>
+      <div className="py-20 w-full flex-col flex items-center gap-20">
+        <div className="max-w-5xl mx-auto px-4 flex flex-col items-center gap-5 mt-40 text-zinc-300">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold">Find Your Next Frontend Internship</h1>
+          <h3 className="text-base md:text-lg lg:text-xl text-center max-w-2xl font-semibold">Discover internships and freelance opportunities built for developers.</h3>
         </div>
-        <div className="min-w-80 flex justify-between items-center text-white font-semibold">
+        <div className="min-w-80 flex flex-col sm:flex-row gap-4 items-center text-white font-semibold">
             <Link to={'/'}><button className="px-4 py-2 bg-white text-black border border-zinc-700 rounded-2xl">Explore Jobs</button></Link>
             <Link to={`/saved-jobs`}><Button variant="outline">View Saved Jobs</Button></Link>
         </div>
