647cfc5688dc3cc6912638e000773fdb9f9c1dbf
Hard1stf <hardikvijeta2@gmail.com>
2026-06-08 17:23:26 +0530

    UI(App/package-lock/package/main/toastMessages): Implement the Toast notaification UI for saved-jobs and removed-jobs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import JobDetails from "./pages/JobDetails";
 import SavedJobs from "./pages/SavedJobs";
 import NotFound from "./pages/NotFound";
 import useLocalStorage from "./hooks/useLocalStorage";
+import toast from "react-hot-toast";
+import { TOAST_MESSAGES } from "./utils/toastMessages";
 
 const App = () => {
   // using custom-hook (useLocalStorage()).
@@ -14,14 +16,16 @@ const App = () => {
       const isSaved = savedJobs.some(job => job.id === jobId);
       if(isSaved){
         setSavedJobs(prev => prev.filter(job => job.id !== jobId))
+        toast.success(TOAST_MESSAGES.JOB_REMOVED)
       }else{
         setSavedJobs(prev => [
           ...prev,
           {
             id: jobId, 
-            savedAt: new Date().toString(),
+            savedAt: new Date().toISOString(),
           }
         ]);
+        toast.success(TOAST_MESSAGES.JOB_SAVED);
       }
     };
     // saving saved jobs in localstorage whenever savedJob changes.
---
+++ src/Utils/toastMessages.js
@@ -0,0 +1,4 @@
+export const TOAST_MESSAGES = {
+  JOB_SAVED: "Job saved successfully",
+  JOB_REMOVED: "Job removed successfully",
+};
