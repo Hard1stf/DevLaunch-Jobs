8ef50b9e8ad06859744b7ca0d69dfe42cce9cf36
Hard1stf <hardikvijeta2@gmail.com>
2026-08-11 01:38:23 +0530

    feat(server): connect backend to MongoDB

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,7 +1,9 @@
-
-
 const port = Number(process.env.PORT) || 4000;
+const mongooseURI = process.env.MONGO_DB_URI;
+
+if (!mongooseURI) throw new Error('MONGO_DB_URI is not defined.');
 
 export const env = {
-    port,
+  port,
+  mongooseURI,
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import 'dotenv/config';
 import app from './app.js';
-import { env } from "./config/env.js";
+import { env } from './config/env.js';
+import { connectDB } from './config/db.js';
 
-app.listen(env.port, () => {
-  console.log(`Server listening on http://localhost:${env.port}`);
-});
+const startServer = async (): Promise<void> => {
+  await connectDB();
+
+  app.listen(env.port, () => {
+    console.log(`Server listening on http://localhost:${env.port}`);
+  });
+};
+
+startServer();
