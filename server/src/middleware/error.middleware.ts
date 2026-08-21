eb27405319ac21eb2add620fe6d9e2cb189077e7
Hard1stf <hardikvijeta2@gmail.com>
2026-08-21 11:23:46 +0530

    feat(server): enhance error middleware to handle Zod validation errors

--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -1,18 +1,32 @@
-import { ErrorRequestHandler } from "express";
-import { APIError } from "../utils/APIError.js";
+import { ErrorRequestHandler } from 'express';
+import { APIError } from '../utils/APIError.js';
+import z from 'zod';
 
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
-    console.error(err);
+  console.error(err);
 
-    if(err instanceof APIError){
-        res.status(err.statusCode).json({
-            success: false,
-            message: err.message,
-        });
-        return;
-    }
-    
-    res.status(500).json({success: false, message: 'Internal server error'});
+  if (err instanceof APIError) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+    return;
+  }
+
+  if (err instanceof z.ZodError) {
+    const message = err.issues.map((issue) => issue.message).join(', ');
+
+    const apiError = new APIError(400, message || 'Invalid Request');
+
+    res.status(apiError.statusCode).json({
+      success: false,
+      message: apiError.message,
+    });
+
+    return;
+  }
+
+  res.status(500).json({ success: false, message: 'Internal server error' });
 };
 
 export default errorMiddleware;
